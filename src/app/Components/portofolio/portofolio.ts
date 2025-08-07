a68e8b60851a81f88011adc95a887c595df3cf42
AlaaMarey <alaamarey616@gmail.com>
2025-08-07 19:49:08 +0300

    Adding Model BotStrap

--- src/app/Components/portofolio/portofolio.ts
+++ src/app/Components/portofolio/portofolio.ts
@@ -8,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class Portofolio {
 
+  imgSrc !: string; 
+
+
+  showImage( imgSrc:string ) {
+    
+    this.imgSrc = imgSrc; 
+    
+
+
+
+  }
+
+
 }
